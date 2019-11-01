f15f27ddf82299bc3e7eb48f4b3fc39dfd397166
Weighting <2286716280@qq.com>
2019-11-01 19:21:51 +0800

    界面

--- GreenSite/urls.py
+++ GreenSite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('green/', include('green.urls')),
+    path('', include('green.urls')),
     path('admin/', admin.site.urls),
 ]
